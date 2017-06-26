7d67ff112540449f5fe68cfa806d98fc08f8dfba
Aleksandrs Levinskis <a.levinskis@gmail.com>
2017-06-26 15:59:43 +0300

    Tools::CalculateRMSE Implemented

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,10 +11,35 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+
+ //reference:https://en.wikipedia.org/wiki/Root-mean-square_deviation
+
+VectorXd rmse(4);
+rmse << 0,0,0,0;
+
+  if (estimations.size() != ground_truth.size()){
+
+    std::cerr << "estimations and ground_truth vectors are not equal"<< std::endl;
+    std::cerr << "In file:"<<__FILE__<<"Line number:"<<__LINE__<< std::endl;
+    return rmse;
+
+  }
+
+  for(int i = 0; i < estimations.size(); i++){
+        VectorXd deviation = estimations[i].array() - ground_truth[i].array();
+        deviation = deviation.array() * deviation.array();
+        rmse = rmse.array() + deviation.array();
+    }
+    //calculate the mean
+    rmse = rmse / estimations.size();
+    //calculate the squared root
+    rmse = rmse.array().sqrt();
+
+
+    return rmse;
+
+
+
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {

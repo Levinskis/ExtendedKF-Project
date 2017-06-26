580286c419594e6369f6198e1afcaa44cca7646a
Aleksandrs Levinskis <a.levinskis@gmail.com>
2017-06-26 19:01:20 +0300

    FusionEKF.cpp update step implemented

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -152,9 +152,19 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    */
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-    // Radar updates
+
+    ekf_.R_ = R_radar_;
+
+    ekf_.x_[0] = measurement_pack.raw_measurements_(0);
+    ekf_.x_[1] = measurement_pack.raw_measurements_(1);
+
+    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+        
   } else {
-    // Laser updates
+     ekf_.R_ = R_laser_;
+     ekf_.H_ = H_laser_;
+     ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
